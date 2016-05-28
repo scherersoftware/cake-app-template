c72789bcc304d2875edfe9e1ce22c5cf6691c254
Robert Scherer <ich@robertscherer.de>
2016-05-28 14:57:31 +0200

    add migration for CakeQueuesadilla jobs table

---
+++ config/Migrations/20160528125702_AddJobsTable.php
@@ -0,0 +1,57 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddJobsTable extends AbstractMigration
+{
+
+    public function up()
+    {
+
+        $this->table('jobs')
+            ->addColumn('queue', 'string', [
+                'default' => null,
+                'limit' => 32,
+                'null' => false,
+            ])
+            ->addColumn('data', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('priority', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('expires_at', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('delay_until', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('locked', 'integer', [
+                'default' => null,
+                'limit' => 1,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'queue',
+                    'locked',
+                ]
+            )
+            ->create();
+
+    }
+
+    public function down()
+    {
+
+        $this->dropTable('jobs');
+    }
+}
+
