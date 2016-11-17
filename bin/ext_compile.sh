3ca1856084e7347166115094dc31e13c0d69dda1
Michael Hoffmann <mh@scherer-software.de>
2016-11-17 14:13:22 +0100

    better admin-lte customisation process

--- bin/ext_compile.sh
+++ bin/ext_compile.sh
@@ -3,11 +3,11 @@
 lessc webroot/less/admin-lte-custom.less webroot/css/admin-lte-custom/admin-lte-custom.css
 lessc webroot/less/app.less webroot/css/app.css
 
-# Compile all skins. The are based on a symlink to bootstrap's less folder in webroot/vendors/AdminLTE
+# Compile all skins
 skins=(skin-blue skin-black skin-yellow skin-green skin-red skin-purple skin-blue-light skin-black-light skin-yellow-light skin-green-light skin-red-light skin-purple-light)
 for skin in ${skins[@]};
 do
-  lessc --clean-css webroot/vendors/AdminLTE/less/skins/${skin}.less webroot/css/admin-lte-custom/skins/${skin}.css
+  lessc --clean-css webroot/vendors/bower_components/admin-lte/build/less/skins/${skin}.less webroot/css/admin-lte-custom/skins/${skin}.css
 done
 
 bin/cake asset_compress build
