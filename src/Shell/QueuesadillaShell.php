7c0eae8ffd7a14fecbf4ab2e9a8872df39b338c5
Michael Hoffmann <mh@scherer-software.de>
2016-11-29 16:53:30 +0100

    Added custom QueuesadillaShell to catch some Worker events and enable setnry logging

---
+++ src/Shell/QueuesadillaShell.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Shell;
+
+use Cake\Datasource\ConnectionManager;
+use Josegonzalez\CakeQueuesadilla\Queue\Queue;
+use Monitor\Error\SentryHandler;
+
+class QueuesadillaShell extends \Josegonzalez\CakeQueuesadilla\Shell\QueuesadillaShell
+{
+    /**
+     * Retrieves a queue worker
+     *
+     * @param \josegonzalez\Queuesadilla\Engine\Base $engine engine to run
+     * @param \Psr\Log\LoggerInterface $logger logger
+     * @return \josegonzalez\Queuesadilla\Worker\Base
+     */
+    public function getWorker($engine, $logger)
+    {
+        $worker = parent::getWorker($engine, $logger);
+        $worker->attachListener('Worker.job.exception', function ($event) {
+            $exception = $event->data['exception'];
+            $exception->job = $event->data['job'];
+            $sentryHandler = new SentryHandler();
+            $sentryHandler->handle($exception);
+        });
+        $worker->attachListener('Worker.job.success', function ($event) {
+            ConnectionManager::get('default')->disconnect();
+        });
+        $worker->attachListener('Worker.job.failure', function ($event) {
+            $failedJob = $event->data['job'];
+            $failedItem = $failedJob->item();
+            $options = [
+                'queue' => 'failed',
+                'failedJob' => $failedJob
+            ];
+            Queue::push($failedItem['class'], $failedJob->data(), $options);
+            ConnectionManager::get('default')->disconnect();
+        });
+        return $worker;
+    }
+}
